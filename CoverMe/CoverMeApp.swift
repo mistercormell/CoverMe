617bb68057f1695191a27dcc0918e7fa4fcd3be7
David Cormell <d.cormell@etoncollege.org.uk>
2023-02-07 09:21:01 +0000

    Initial Commit

---
+++ CoverMe/CoverMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverMeApp.swift
+//  CoverMe
+//
+//  Created by Cormell, David - DPC on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoverMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
