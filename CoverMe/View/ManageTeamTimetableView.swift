2b1d7f8c1684ae00cdae770b944de46fdb3a2452
David Cormell <d.cormell@etoncollege.org.uk>
2024-09-20 16:53:03 +0100

    Adding timetable changes for classics and divinity as part of latest release

---
+++ CoverMe/View/ManageTeamTimetableView.swift
@@ -0,0 +1,18 @@
+//
+//  ManageTeamTimetableView.swift
+//  CoverMe
+//
+//  Created by Cormell, David - DPC on 12/09/2024.
+//
+
+import SwiftUI
+
+struct ManageTeamTimetableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManageTeamTimetableView()
+}
---
+++ CoverMe/View/ManageTeamView.swift
@@ -0,0 +1,18 @@
+//
+//  ManageTeamView.swift
+//  CoverMe
+//
+//  Created by Cormell, David - DPC on 12/09/2024.
+//
+
+import SwiftUI
+
+struct ManageTeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ManageTeamView()
+}
